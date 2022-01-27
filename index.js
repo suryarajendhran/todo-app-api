7a45a2c16b25660efeb2c432d5aa17b3f8c366fd
Surya Rajendhran <32726562+suryarajendhran@users.noreply.github.com>
2022-01-27 21:10:53 +0530

    init repo

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = require('./routes');
+
+const app = express();
+app.use(express.json());
+app.use(router);
+
+app.listen(8080, () => {
+    console.log('Server has started');
+})

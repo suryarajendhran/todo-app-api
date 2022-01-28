766cb839383b64ef942debceb7681c9e0415a164
Surya Rajendhran <32726562+suryarajendhran@users.noreply.github.com>
2022-01-28 14:32:24 +0530

    feat: allow cors

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = require('./routes');
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
+app.options('*', cors()); 
 app.use(express.json());
 app.use(router);
+
 
 app.listen(8080, () => {
     console.log('Server has started');

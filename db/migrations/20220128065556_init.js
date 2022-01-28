0ad8967091d866bcbbdfd1e80d4999a70fb2d984
Surya Rajendhran <32726562+suryarajendhran@users.noreply.github.com>
2022-01-28 12:33:39 +0530

    db: first migration

---
+++ db/migrations/20220128065556_init.js
@@ -0,0 +1,20 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = async function(knex) {
+await knex.schema.createTable('todos', async (table) => {
+    table.uuid("id").primary().defaultTo(knex.raw("gen_random_uuid()"));
+    table.string('description', 255).notNullable();
+    table.boolean('completed').defaultTo(false);
+    table.timestamps(true, true);
+})
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = async function(knex) {
+  await knex.schema.dropTable('todos');
+};

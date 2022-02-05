2fc23c6f320352d452ae252c1e6929ce335614cf
Surya Rajendhran <32726562+suryarajendhran@users.noreply.github.com>
2022-02-06 03:46:27 +0530

    update table

--- db/migrations/20220128065556_init.js
+++ db/migrations/20220128065556_init.js
@@ -2,19 +2,19 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = async function(knex) {
-await knex.schema.createTable('todos', async (table) => {
+exports.up = async function (knex) {
+  await knex.schema.createTable("todos", async (table) => {
     table.uuid("id").primary().defaultTo(knex.raw("gen_random_uuid()"));
-    table.string('description', 255).notNullable();
-    table.boolean('completed').defaultTo(false);
+    table.string("description", 255).notNullable();
+    table.boolean("completed").defaultTo(false);
     table.timestamps(true, true);
-})
+  });
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = async function(knex) {
-  await knex.schema.dropTable('todos');
+exports.down = async function (knex) {
+  await knex.schema.dropTable("todos");
 };
---
+++ db/migrations/20220205211710_add_uid.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = async function (knex) {
+  await knex.schema.alterTable("todos", async (table) => {
+    table.string("uid");
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = async function (knex) {
+  await knex.schema.alterTable("todos", async (table) => {
+    table.dropColumn("uid");
+  });
+};
